a3d917e1831a05022b06a33790bfbd34ba2b4ec7
Cristian <jazhiel.st@gmail.com>
2018-07-05 01:26:52 +0200

    Implement voting!

--- src/components/Poll.vue
+++ src/components/Poll.vue
@@ -1,28 +1,46 @@
 <template>
-  <div class="poll-container" v-if="items.length > 0">
+  <div
+    class="poll-container"
+    v-if="items.length > 0">
     <h2>{{ text }}</h2>
     <div class="poll-main-content">
       <div class="poll-choices">
         <div
           class="single-choice"
           v-for="(item, index) in items"
+          v-bind:class="{ selected : selectedChoice === index}"
+          @click="makeSelection(index)"
           :key="index">
-          <div><span>{{ item.label }}</span> : {{ item.votes }} votes </div>
+          <div><span class="choice-label">{{ item.label }}</span>: {{ item.votes }} votes </div>
           <div
-            class="choice-percentage"
-            v-bind:style="{ width: calculatePercentage(item.votes)}"
-            >
-            {{ parseFloat(item.votes / totalVotes * 100).toFixed(2) }}
+            class="choice-percentage-wrapper"
+            v-if="item.votes">
+            <div
+              class="choice-percentage"
+              v-bind:style="{ width: calculatePercentage(item.votes)}"
+            />
+            <span class="percentage-number">
+              {{ item.votes ? parseFloat(item.votes / totalVotes * 100).toFixed(2) + '%' : 0 }}
+            </span>
           </div>
         </div>
-        <div v-if="totalVotes">
-          total votes: {{ totalVotes }}
+        <div
+          v-if="totalVotes"
+          class="poll-total">
+          Total Votes: <span class="total-counter">{{ totalVotes }}</span>
         </div>
       </div>
-      
+
       <PollPieChart
+        v-if="totalVotes"
         v-bind:items="items"
       />
+    </div>
+    <div class="poll-actions">
+      <md-button
+        class="md-raised md-primary"
+        @click="vote"
+      >Vote</md-button>
     </div>
   </div>
 </template>
@@ -37,6 +55,7 @@ export default {
   },
   data() {
     return {
+      selectedChoice: null,
       createdAt: '',
       text: '',
       items: [],
@@ -44,8 +63,33 @@ export default {
     };
   },
   methods: {
+    vote() {
+      if (this.selectedChoice === null) {
+        alert('You need to pick a choise first');
+      } else {
+        this.items[this.selectedChoice].votes += 1;
+        const pollId = this.$route.params.id;
+        const ref = db.collection('polls').doc(pollId);
+
+        ref.update({
+          options: this.items,
+        }).then((r) => {
+          setTimeout(() => {
+            alert("Thanks for voting");
+          }, 1000);
+        }).catch((e) => {
+          console.log(e);
+        });
+      }
+    },
     calculatePercentage(votes) {
-      return `${parseFloat((votes / this.totalVotes) * 100).toFixed(2)}%`;
+      if (votes > 0) {
+        return `${parseFloat((votes / this.totalVotes) * 100).toFixed(2)}%`;
+      }
+      return '0%';
+    },
+    makeSelection(index) {
+      this.selectedChoice = index;
     },
   },
   created() {
@@ -75,6 +119,7 @@ export default {
   align-items: center;
 }
 .poll-main-content {
+  padding: 30px 20px;
   @media @desktop, @desktop-xl {
     display: flex;
   }
@@ -83,13 +128,46 @@ export default {
     min-width: 960px;
   }
   .poll-choices {
-    .choice-percentage {
-      //height: 4px;
-      background-color: red;
-      -webkit-transition: width 1s ease-in-out;
-      -moz-transition: width 1s ease-in-out;
-      -o-transition: width 1s ease-in-out;
-      transition: width 1s ease-in-out;
+    .single-choice {
+      margin-bottom: 20px;
+      opacity: 0.9;
+      padding: 10px;
+      &:hover {
+        opacity: 1;
+        background: #eee;
+      }
+      &.selected {
+        border: 1px solid#448aff;;
+        background: #eee;
+        padding: 9px;
+      }
+      cursor: pointer;
+      .choice-label {
+        font-weight: 600;
+      }
+      .choice-percentage-wrapper {
+        display: flex;
+        margin: 10px 0;
+        .choice-percentage {
+          background-color: #4BA6A8;
+          height: 25px;
+          -webkit-transition: width 1s ease-in-out;
+          -moz-transition: width 1s ease-in-out;
+          -o-transition: width 1s ease-in-out;
+          transition: width 1s ease-in-out;
+        }
+        .percentage-number {
+          margin-left: 5px;
+          line-height: 25px;
+        }
+      }
+    }
+    .poll-total {
+      margin-top: 20px;
+      margin-left: 10px;
+      .total-counter {
+        font-weight: 600;
+      }
     }
   }
   > div {
